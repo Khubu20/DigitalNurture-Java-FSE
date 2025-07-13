69c8dde8a4a799ff18e9c56d164b998aa2357b96
khubu20 <khubu.gupta20@gmail.com>
2025-07-13 19:48:23 +0530

    implementing week 4 handson

--- week4_Spring_JWT/Create authentication service that returns JWT/springlearn/src/main/java/com/cognizant/springlearn/util/JwtUtil.java
+++ week4_Spring_JWT/Create authentication service that returns JWT/springlearn/src/main/java/com/cognizant/springlearn/util/JwtUtil.java
@@ -1,23 +1,25 @@
 package com.cognizant.springlearn.util;
 
+import java.util.Date;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 
-import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "secret";
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) // 15 minutes
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour validity
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
 }
-
