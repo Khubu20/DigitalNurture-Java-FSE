8df69b7b54d3ac31433346952dba6e55eeedc6a2
khubu20 <khubu.gupta20@gmail.com>
2025-07-20 18:51:14 +0530

    Implementing week 5 handson

---
+++ Week5_MicrosServices/Creating_Microservices_for_Account&Loan/1234567/microservices/account&loan/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.account;
+
+import com.cognizant.loan.LoanApplication;
+import org.springframework.boot.SpringApplication;
+
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+
+}
